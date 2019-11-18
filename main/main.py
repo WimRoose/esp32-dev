32b7e50d6153afc0b3415a9005aa5eed7fc39510
Wim Roose <wimroo@gmail.com>
2019-11-18 22:11:28 +0100

    boilercode-test

--- main/main.py
+++ main/main.py
@@ -6,9 +6,9 @@ def download_and_install_update_if_available():
     ota_updater.download_and_install_update_if_available('Los Borrachos', 'xxxxxxxxxxxx')
 
 def start():
-	while True:
-		print("helloo-")
-		utime.sleep(2)
+    while True:
+        print("helloo-")
+        utime.sleep(2)
     # your custom code goes here. Something like this: ...
     # from main.x import YourProject
     # project = YourProject()
