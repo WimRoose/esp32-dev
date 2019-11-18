0e3e7604ea1f388416121c70e0fb2648c70b82aa
Wim Roose <wimroo@gmail.com>
2019-11-18 22:26:15 +0100

    boilercode-test

---
+++ main.py
@@ -0,0 +1,20 @@
+from main.ota_updater import OTAUpdater
+import utime
+
+def download_and_install_update_if_available():
+    ota_updater = OTAUpdater('https://github.com/WimRoose/esp32-dev')
+    ota_updater.download_and_install_update_if_available('Los Borrachos', 'xxxxxxxxxxxx')
+
+def start():
+    
+    # your custom code goes here. Something like this: ...
+    # from main.x import YourProject
+    # project = YourProject()
+    # ...
+
+def boot():
+    download_and_install_update_if_available()
+    start()
+
+
+boot()
